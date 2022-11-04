9cbe3cf0d59d4ca11b3868b85871479b793ddac5
Furkan Cemal Çalışkan <furkanc.caliskan@icloud.com>
2022-11-04 22:41:23 +0300

    Initial Commit

---
+++ AsyncImageSwiftUI/AsyncImageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageSwiftUIApp.swift
+//  AsyncImageSwiftUI
+//
+//  Created by Furkan Cemal Çalışkan on 4.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
